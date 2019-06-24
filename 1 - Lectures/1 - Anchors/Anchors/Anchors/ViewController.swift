424ee0ebf796b85a273ea59b7648f8d747ac76ae
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2019-06-24 09:19:06 -0600

    Add first anchor screen

---
+++ 1 - Lectures/1 - Anchors/Anchors/Anchors/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Anchors
+//
+//  Created by Jonathan Rasmusson (Contractor) on 2019-06-24.
+//  Copyright © 2019 Jonathan Rasmusson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavigationItems()
+        setupViews()
+    }
+
+    func setupViews() {
+        let upperLeftLabel = makeLabel(withText: "upperLeft")
+        let upperRightLabel = makeLabel(withText: "upperRight")
+        let lowerLeftLabel = makeLabel(withText: "lowerLeft")
+        let lowerRightLabel = makeLabel(withText: "lowerRight")
+
+        view.addSubview(upperLeftLabel)
+        view.addSubview(upperRightLabel)
+        view.addSubview(lowerLeftLabel)
+        view.addSubview(lowerRightLabel)
+
+        upperLeftLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+
+        upperRightLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true            // don't use `leftAnchor`
+        upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true   // negative sign
+
+        lowerLeftLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        lowerLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+
+        lowerRightLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        lowerRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+    }
+
+    func setupNavigationItems() {
+        navigationItem.title = "Anchors"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "SafeAreas", style: .plain, target: self, action: #selector(handleSafeAreas))
+    }
+
+    // MARK: - Actions
+    @objc func handleSafeAreas() {
+//        let createCompanyController = CreateCompanyController()
+//        createCompanyController.delegate = self
+//
+//        let navController = CustomNavigationController(rootViewController: createCompanyController)
+//
+//        present(navController, animated: true, completion: nil)
+    }
+
+    // MARK: - Factory methods
+
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false // important!
+        label.backgroundColor = .yellow
+        label.text = text
+
+        return label
+    }
+}
+
