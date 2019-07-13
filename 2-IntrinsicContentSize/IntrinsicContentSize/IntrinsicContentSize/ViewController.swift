74cf26b9c820d1b3477e341e88d562231ad506d3
jrasmusson <rasmus4200@gmail.com>
2019-07-13 10:54:16 -0600

    cleanup

---
+++ 2-IntrinsicContentSize/IntrinsicContentSize/IntrinsicContentSize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntrinsicContentSize
+//
+//  Created by Jonathan Rasmusson Work Pro on 2019-07-13.
+//  Copyright © 2019 Rasmusson Software Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
