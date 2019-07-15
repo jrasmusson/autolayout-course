bec63734c7c52edf773ac589de81b59b0834229c
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2019-07-15 08:54:16 -0600

    Start design challenge anchors

--- 1-AnchorsLayoutGuides/DesignChallenge/DesignChallenge/AppDelegate.swift
+++ 1-AnchorsLayoutGuides/DesignChallenge/DesignChallenge/AppDelegate.swift
@@ -13,9 +13,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .black
+        window?.makeKeyAndVisible()
+
+        let navigatorController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigatorController
+
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = .offBlack
+
         return true
     }
 
---
+++ 1-AnchorsLayoutGuides/DesignChallenge/DesignChallenge/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  DesignChallenge
+//
+//  Created by Jonathan Rasmusson (Contractor) on 2019-07-15.
+//  Copyright © 2019 Jonathan Rasmusson. All rights reserved.
+//
+
+import UIKit
+
+// 3
+extension UIColor {
+    static let offBlack = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+}
