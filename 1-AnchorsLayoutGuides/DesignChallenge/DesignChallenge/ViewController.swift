4b7ee5a46ede5982535b278d0249f1bf7e575c3b
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2019-07-15 08:27:33 -0600

    cleanup

---
+++ 1-AnchorsLayoutGuides/DesignChallenge/DesignChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignChallenge
+//
+//  Created by Jonathan Rasmusson (Contractor) on 2019-07-15.
+//  Copyright © 2019 Jonathan Rasmusson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
