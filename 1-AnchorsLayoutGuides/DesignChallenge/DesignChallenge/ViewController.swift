a56be180f66907cc8741ef42e937647dcee989e5
jrasmusson <rasmus4200@gmail.com>
2019-07-19 08:18:20 -0600

    Design challenge WIP

--- 1-AnchorsLayoutGuides/DesignChallenge/DesignChallenge/ViewController.swift
+++ 1-AnchorsLayoutGuides/DesignChallenge/DesignChallenge/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let margin: CGFloat = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
@@ -23,34 +25,96 @@ class ViewController: UIViewController {
     func setupViews() {
         let offlineLabel = makeLabel(withText: "Offline")
         let offlineSwitch = makeSwitch(isOne: false)
-
+        let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
+        
+        let crossfadeLabel = makeBoldLabel(withText: "Crossfade")
+        let crossfadeMinLabel = makeSubLabel(withText: "0s")
+        let crossfadeMaxLabel = makeSubLabel(withText: "12s")
+        let crossfadeProgressView = makeProgressView()
+        
         view.addSubview(offlineLabel)
         view.addSubview(offlineSwitch)
-
-        offlineLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
-        offlineLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        view.addSubview(offlineSublabel)
+        
+        view.addSubview(crossfadeLabel)
+        view.addSubview(crossfadeMinLabel)
+        view.addSubview(crossfadeProgressView)
+        view.addSubview(crossfadeMaxLabel)
+        
+        offlineLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: margin).isActive = true
+        offlineLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin).isActive = true
 
         offlineSwitch.centerYAnchor.constraint(equalTo: offlineLabel.centerYAnchor).isActive = true
-        offlineSwitch.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        offlineSwitch.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin).isActive = true
+        
+        offlineSublabel.topAnchor.constraint(equalTo: offlineLabel.bottomAnchor, constant: margin).isActive = true
+        offlineSublabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin).isActive = true
+        offlineSublabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin).isActive = true
+        
+        crossfadeLabel.topAnchor.constraint(equalTo: offlineSublabel.bottomAnchor, constant: 32).isActive = true
+        crossfadeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        crossfadeMinLabel.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: 32).isActive = true
+        crossfadeMinLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin).isActive = true
+        
+        crossfadeProgressView.centerYAnchor.constraint(equalTo: crossfadeMinLabel.centerYAnchor).isActive = true
+        crossfadeProgressView.leadingAnchor.constraint(equalTo: crossfadeMinLabel.trailingAnchor, constant: 4).isActive = true
+        crossfadeProgressView.trailingAnchor.constraint(equalTo: crossfadeMaxLabel.leadingAnchor, constant: -4).isActive = true
+        
+        crossfadeMaxLabel.centerYAnchor.constraint(equalTo: crossfadeMinLabel.centerYAnchor).isActive = true
+        crossfadeMaxLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin).isActive = true
+
     }
-}
+    
+    // MARK: - Factories
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 17)
+        
+        return label
+    }
+    
+    func makeSubLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.numberOfLines = 0 // multiline
+        
+        return label
+    }
+    
+    func makeBoldLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        
+        return label
+    }
+    
+    func makeSwitch(isOne: Bool) -> UISwitch {
+        let theSwitch = UISwitch()
+        theSwitch.translatesAutoresizingMaskIntoConstraints = false
+        theSwitch.isOn = isOne
+        
+        return theSwitch
+    }
+    
+    func makeProgressView() -> UIProgressView {
+        let progressView = UIProgressView(progressViewStyle: .default)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.tintColor = .gray
+        
+        return progressView
+    }
 
-func makeLabel(withText text: String) -> UILabel {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    label.text = text
-    label.textColor = .white
-    label.font = UIFont.systemFont(ofSize: 17)
-
-    return label
-}
-
-func makeSwitch(isOne: Bool) -> UISwitch {
-    let theSwitch = UISwitch()
-    theSwitch.translatesAutoresizingMaskIntoConstraints = false
-    theSwitch.isOn = isOne
-
-    return theSwitch
 }
 
 extension UINavigationController {
