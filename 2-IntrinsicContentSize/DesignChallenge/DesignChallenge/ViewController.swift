5a73ae8891fcf8770a3f53e4670adaa0630b9fb2
jrasmusson <rasmus4200@gmail.com>
2019-07-22 07:31:19 -0600

    cleanup

--- 2-IntrinsicContentSize/DesignChallenge/DesignChallenge/ViewController.swift
+++ 2-IntrinsicContentSize/DesignChallenge/DesignChallenge/ViewController.swift
@@ -12,9 +12,81 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews() {
+        let image = makeImageView(named: "rush")
+        let titleLabel = makeTitleLabel(withText: "Tom Sawyer")
+        let albumLabel = makeAlbumLabel(withText: "Rush · Moving Pictures (2011 Remaster)")
+        let button = makeButton(withText: "PLAY ON SPOTIFY")
+        
+        view.addSubview(image)
+        view.addSubview(titleLabel)
+        view.addSubview(albumLabel)
+        view.addSubview(button)
+        
+        image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        image.heightAnchor.constraint(equalTo: image.widthAnchor, multiplier: 1).isActive = true
+        image.widthAnchor.constraint(equalToConstant: view.bounds.width).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 8).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+
+        albumLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        albumLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        albumLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+
+        button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    public func makeImageView(named: String) -> UIImageView {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: named)
+        
+        // By making the image hug itself a little bit less and less resistant to being compressed
+        // we allow the image to stretch and grow as required
+        view.setContentHuggingPriority(UILayoutPriority(rawValue: 249), for: .vertical)
+        view.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 749), for: .vertical)
+        
+        return view
+    }
+    
+    func makeTitleLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.backgroundColor = .yellow
+        
+        return label
     }
 
+    func makeAlbumLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .lightGray
+        label.backgroundColor = .yellow
+        
+        return label
+    }
 
+    func makeButton(withText text: String) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .blue
+        button.contentEdgeInsets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        return button
+    }
 }
-
