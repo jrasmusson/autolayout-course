d583461e7d535d3e384c5b0dfa7b7d8f917cf47a
jrasmusson <rasmus4200@gmail.com>
2019-07-22 06:38:37 -0600

    Intrinsic content size design challenge shell

---
+++ 2-IntrinsicContentSize/DesignChallenge/DesignChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignChallenge
+//
+//  Created by Jonathan Rasmusson Work Pro on 2019-07-22.
+//  Copyright © 2019 Rasmusson Software Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
