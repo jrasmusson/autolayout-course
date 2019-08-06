4ba3b567f308e10ecb1d029d1046850f4de6a441
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2019-08-06 07:58:24 -0600

    cleanup

--- 3-Stacks/StackViews/StackViews/Simple.swift
+++ 3-Stacks/StackViews/StackViews/Simple.swift
@@ -9,5 +9,28 @@
 import UIKit
 
 class Simple: UIViewController {
-    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+    func setupViews() {
+
+        let emailTextField = makeTextField(withPlaceholderText: "email address")
+        let passwordTextField = makeTextField(withPlaceholderText: "password")
+        let loginButton = makeButton(withText: "Login")
+
+        let stackView = makeStackView(withOrientation: .vertical)
+        stackView.addArrangedSubview(emailTextField)
+        stackView.addArrangedSubview(passwordTextField)
+        stackView.addArrangedSubview(loginButton)
+
+        view.addSubview(stackView)
+
+        // just going to center in view - remember stackViews by themselves have no size - they get it from their contents
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
 }
