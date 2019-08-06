9aea09d02126ea13406ca86a3d09b311ec9bf8fd
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2019-08-06 07:39:48 -0600

    Create simple stackview project

---
+++ 3-Stacks/StackViews/StackViews/Simple.swift
@@ -0,0 +1,13 @@
+//
+//  Simple.swift
+//  StackViews
+//
+//  Created by Jonathan Rasmusson (Contractor) on 2019-08-06.
+//  Copyright © 2019 Jonathan Rasmusson. All rights reserved.
+//
+
+import UIKit
+
+class Simple: UIViewController {
+    
+}
