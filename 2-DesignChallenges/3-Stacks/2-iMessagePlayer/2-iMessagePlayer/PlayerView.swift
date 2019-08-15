e4803b039640c0acbf8818c65db77e85c1d88368
jrasmusson <rasmus4200@gmail.com>
2019-08-15 06:22:45 -0700

    Extracted playerView

--- 2-DesignChallenges/3-Stacks/2-iMessagePlayer/2-iMessagePlayer/PlayerView.swift
+++ 2-DesignChallenges/3-Stacks/2-iMessagePlayer/2-iMessagePlayer/PlayerView.swift
@@ -1,8 +1,8 @@
 //
-//  PlayerView.swift
+//  PreviewPlayer.swift
 //  2-iMessagePlayer
 //
-//  Created by Jonathan Rasmusson Work Pro on 2019-08-11.
+//  Created by Jonathan Rasmusson Work Pro on 2019-08-14.
 //  Copyright © 2019 Rasmusson Software Consulting. All rights reserved.
 //
 
@@ -20,38 +20,65 @@ class PlayerView: UIView {
     }
     
     func setupViews() {
-        let playButton = makePlayButton()
-        let previewStartLabel = makePreviewLabel(withText: "0:00")
-        let previewEndLabel = makePreviewLabel(withText: "0:30")
-        let progressView = makeProgressView()
+        let stackView = makeStackView(withOrientation: .vertical)
         
-        addSubview(playButton)
-        addSubview(previewStartLabel)
-        addSubview(progressView)
-        addSubview(previewEndLabel)
+        let trackLabel = makeTrackLabel(withText: "Tom Sawyer")
+        let albumLabel = makeAlbumLabel(withText: "Rush • Moving Pictures (2011 Remaster)")
+        let playerView = ProgressRow()
+        let spotifyButtonView = makeSpotifyButtonStackView()
+        // let spotifyButtonView = makeSpotifyButtonCustomView() // Alternative
         
-        playButton.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        playButton.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        playButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
-        playButton.widthAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        addSubview(stackView)
         
-        previewStartLabel.centerYAnchor.constraint(equalTo: playButton.centerYAnchor).isActive = true
-        previewStartLabel.leadingAnchor.constraint(equalTo: playButton.trailingAnchor, constant: 8).isActive = true
-        
-        progressView.centerYAnchor.constraint(equalTo: playButton.centerYAnchor).isActive = true
-        progressView.leadingAnchor.constraint(equalTo: previewStartLabel.trailingAnchor, constant: 8).isActive = true
-        
-        previewEndLabel.centerYAnchor.constraint(equalTo: playButton.centerYAnchor).isActive = true
-        previewEndLabel.leadingAnchor.constraint(equalTo: progressView.trailingAnchor, constant: 8).isActive = true
-        previewEndLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        stackView.addArrangedSubview(trackLabel)
+        stackView.addArrangedSubview(albumLabel)
+        stackView.addArrangedSubview(playerView)
+        stackView.addArrangedSubview(spotifyButtonView)
+
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
     
+    //
+    // Spotify button width
+    //
+    //    Getting the Spotify button, not spreading across the entire width of the phone is a challenge.
+    //    The StackView wants to 'fill' the entire width. But we don't want that. Two ways to handle.
+    
+    // 1. Stick the button into another StackView and set it's padding via margins.
+    func makeSpotifyButtonStackView() -> UIStackView {
+        let spotifyButton = makeSpotifyButton(withText: "PLAY ON SPOTIFY")
+        
+        let spotifyButtonStackView = makeStackView(withOrientation: .vertical)
+        spotifyButtonStackView.isLayoutMarginsRelativeArrangement = true
+        spotifyButtonStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40)
+        
+        spotifyButtonStackView.addArrangedSubview(spotifyButton)
+        
+        return spotifyButtonStackView
+    }
+    
+    // 2. Create a custom view, set constraints on the button in there, and then add it to the stackView.
+    func makeSpotifyButtonCustomView() -> UIView {
+        let spotifyButton = makeSpotifyButton(withText: "PLAY ON SPOTIFY")
+        
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        container.addSubview(spotifyButton)
+        
+        spotifyButton.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        spotifyButton.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+        spotifyButton.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 24).isActive = true
+        spotifyButton.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -24).isActive = true
+        spotifyButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        
+        return container
+    }
+
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: buttonHeight)
+        return CGSize(width: UIView.noIntrinsicMetric, height: 200)
     }
 }
-
-// Notes:
-//
-// 1. This control I did design flush to the edges (padding provided by stackView)
-
--- 2-DesignChallenges/3-Stacks/2-iMessagePlayer/2-iMessagePlayer/Solution.swift
+++ 2-DesignChallenges/3-Stacks/2-iMessagePlayer/2-iMessagePlayer/Solution.swift
@@ -25,17 +25,9 @@ class Solution: UIViewController {
         
         // bottom
         let bottomStackView = makeBottomStackView()
-
-        let trackLabel = makeTrackLabel(withText: "Tom Sawyer")
-        let albumLabel = makeAlbumLabel(withText: "Rush • Moving Pictures (2011 Remaster)")
         let playerView = PlayerView()
-        let spotifyButtonView = makeSpotifyButtonStackView()
-        // let spotifyButtonView = makeSpotifyButtonCustomView() // Alternative
         
-        bottomStackView.addArrangedSubview(trackLabel)
-        bottomStackView.addArrangedSubview(albumLabel)
         bottomStackView.addArrangedSubview(playerView)
-        bottomStackView.addArrangedSubview(spotifyButtonView)
         
         // container
         let containerStackView = makeStackView(withOrientation: .vertical)
@@ -65,47 +57,7 @@ class Solution: UIViewController {
         let bottomStackView = makeStackView(withOrientation: .vertical)
         bottomStackView.isLayoutMarginsRelativeArrangement = true
         bottomStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8)
-
+        
         return bottomStackView
     }
-    
-    //
-    // Spotify button width
-    //
-    //    Getting the Spotify button, not spreading across the entire width of the phone is a challenge.
-    //    The StackView wants to 'fill' the entire width. But we don't want that. Two ways to handle.
-
-    // 1. Stick the button into another StackView and set it's padding via margins.
-    func makeSpotifyButtonStackView() -> UIStackView {
-        let spotifyButton = makeSpotifyButton(withText: "PLAY ON SPOTIFY")
-        
-        let spotifyButtonStackView = makeStackView(withOrientation: .vertical)
-        spotifyButtonStackView.isLayoutMarginsRelativeArrangement = true
-        spotifyButtonStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40)
-        
-        spotifyButtonStackView.addArrangedSubview(spotifyButton)
-        
-        return spotifyButtonStackView
-    }
-    
-    // 2. Create a custom view, set constraints on the button in there, and then add it to the stackView.
-    func makeSpotifyButtonCustomView() -> UIView {
-        let spotifyButton = makeSpotifyButton(withText: "PLAY ON SPOTIFY")
-        
-        let container = UIView()
-        container.translatesAutoresizingMaskIntoConstraints = false
-        container.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
-        container.addSubview(spotifyButton)
-        
-        spotifyButton.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
-        spotifyButton.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
-        spotifyButton.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 24).isActive = true
-        spotifyButton.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -24).isActive = true
-        spotifyButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
-        
-        return container
-    }
-
 }
-
-
