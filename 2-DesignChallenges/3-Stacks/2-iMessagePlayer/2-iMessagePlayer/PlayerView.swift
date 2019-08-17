d1671e449ce4f01fc736d0576404fd43e5c4ed75
jrasmusson <rasmus4200@gmail.com>
2019-08-17 07:15:00 -0700

    Migrated to spacer solution

--- 2-DesignChallenges/3-Stacks/2-iMessagePlayer/2-iMessagePlayer/PlayerView.swift
+++ 2-DesignChallenges/3-Stacks/2-iMessagePlayer/2-iMessagePlayer/PlayerView.swift
@@ -10,19 +10,24 @@ import UIKit
 
 class PlayerView: UIView {
     
-    var stackView: UIStackView
-    var topAnchorConstraint = NSLayoutConstraint()
-    var bottomAnchorConstraint = NSLayoutConstraint()
-    var centerYConstraint = NSLayoutConstraint()
+//    var stackView: UIStackView
+//    var topAnchorConstraint = NSLayoutConstraint()
+//    var bottomAnchorConstraint = NSLayoutConstraint()
+//    var centerYConstraint = NSLayoutConstraint()
+
+    var spacer1: UIView
+    var spacer2: UIView
 
     init() {
-        stackView = makeStackView(withOrientation: .vertical)
-        stackView.distribution = .fillProportionally
+        spacer1 = makeSpacerView(height: 100)
+        spacer1.backgroundColor = .red
+        spacer2 = makeSpacerView(height: 100)
+        spacer2.backgroundColor = .blue
         
         super.init(frame: .zero)
         
         setupViews()
-        setupOrientationConstraints()
+//        setupOrientationConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,42 +35,51 @@ class PlayerView: UIView {
     }
     
     func setupViews() {
+        let stackView = makeStackView(withOrientation: .vertical)
+        stackView.distribution = .fillProportionally
+
         let trackLabel = makeTrackLabel(withText: "Tom Sawyer")
         let albumLabel = makeAlbumLabel(withText: "Rush • Moving Pictures (2011 Remaster)")
         let playerView = ProgressRow()
         let spotifyButtonView = makeSpotifyButtonCustomView()
         
+
         addSubview(stackView)
         
+        stackView.addArrangedSubview(spacer1)
         stackView.addArrangedSubview(trackLabel)
         stackView.addArrangedSubview(albumLabel)
         stackView.addArrangedSubview(playerView)
         stackView.addArrangedSubview(spotifyButtonView)
+        stackView.addArrangedSubview(spacer2)
         
-        // topAnchor & bottom are dynamic - see below
+        spacer1.heightAnchor.constraint(equalTo: spacer2.heightAnchor).isActive = true
+
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
     
     //
     // When the orientation changes, we want to turn off the top & bottom constraints
     // and center the playerView on the y-axis.
     //
-    func setupOrientationConstraints() {
-        topAnchorConstraint = stackView.topAnchor.constraint(equalTo: topAnchor)
-        bottomAnchorConstraint = stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        centerYConstraint = stackView.centerYAnchor.constraint(equalTo: centerYAnchor)
-        
-        if UIDevice.current.orientation.isPortrait {
-            topAnchorConstraint.isActive = true
-            bottomAnchorConstraint.isActive = true
-            centerYConstraint.isActive = false
-        } else {
-            topAnchorConstraint.isActive = false
-            bottomAnchorConstraint.isActive = false
-            centerYConstraint.isActive = true
-        }
-    }
+//    func setupOrientationConstraints() {
+//        topAnchorConstraint = stackView.topAnchor.constraint(equalTo: topAnchor)
+//        bottomAnchorConstraint = stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+//        centerYConstraint = stackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+//
+//        if UIDevice.current.orientation.isPortrait {
+//            topAnchorConstraint.isActive = true
+//            bottomAnchorConstraint.isActive = true
+//            centerYConstraint.isActive = false
+//        } else {
+//            topAnchorConstraint.isActive = false
+//            bottomAnchorConstraint.isActive = false
+//            centerYConstraint.isActive = true
+//        }
+//    }
     
     //
     // This customView centers the button and allows it to size itself.
@@ -91,11 +105,11 @@ class PlayerView: UIView {
     
     func adjustForOrientiation() {
         if UIDevice.current.orientation.isLandscape {
-            topAnchorConstraint.isActive = false
-            centerYConstraint.isActive = true
+            spacer1.isHidden = false
+            spacer2.isHidden = false
         } else {
-            topAnchorConstraint.isActive = true
-            centerYConstraint.isActive = false
+            spacer1.isHidden = true
+            spacer2.isHidden = true
         }
     }
 }
