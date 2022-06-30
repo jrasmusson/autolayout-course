fce7b35c06024fd6afe9c33af5112778823ad676
Oscar Cristaldo <fabianskier@icloud.com>
2022-06-29 22:01:47 -0400

    Add anchor challenge
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- DesignChallenges/1-Anchors/BasicAnchors/BasicAnchors/Challenge.swift
+++ DesignChallenges/1-Anchors/BasicAnchors/BasicAnchors/Challenge.swift
@@ -9,61 +9,113 @@
 import UIKit
 
 class Challenge: UIViewController {
-
+    
     let margin: CGFloat = 20
     let spacing: CGFloat = 32
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         setupViews()
     }
-
+    
     func setupNavigationBar() {
         navigationItem.title = "Playback"
     }
-
+    
     func setupViews() {
-
+        
         // Comment in incrementally ...
-        //        let offlineLabel = makeLabel(withText: "Offline")
-        //        let offlineSwitch = makeSwitch(isOne: false)
-        //        let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
-        //
-        //        let crossfadeLabel = makeBoldLabel(withText: "Crossfade")
-        //        let crossfadeMinLabel = makeSubLabel(withText: "0s")
-        //        let crossfadeMaxLabel = makeSubLabel(withText: "12s")
-        //        let crossfadeProgressView = makeProgressView()
-        //
-        //        let gaplessPlaybackLabel = makeLabel(withText: "Gapless Playback")
-        //        let gaplessPlaybackSwitch = makeSwitch(isOne: true)
-        //
-        //        let hideSongsLabel = makeLabel(withText: "Hide Unplayable Songs")
-        //        let hideSongsSwitch = makeSwitch(isOne: true)
-        //
-        //        let enableNormalizationLabel = makeLabel(withText: "Enable Audio Normalization")
-        //        let enableNormalizationSwitch = makeSwitch(isOne: true)
-        //
-        //        view.addSubview(offlineLabel)
-        //        view.addSubview(offlineSwitch)
-        //        view.addSubview(offlineSublabel)
-        //
-        //        view.addSubview(crossfadeLabel)
-        //        view.addSubview(crossfadeMinLabel)
-        //        view.addSubview(crossfadeProgressView)
-        //        view.addSubview(crossfadeMaxLabel)
-        //
-        //        view.addSubview(gaplessPlaybackLabel)
-        //        view.addSubview(gaplessPlaybackSwitch)
-        //
-        //        view.addSubview(hideSongsLabel)
-        //        view.addSubview(hideSongsSwitch)
-        //
-        //        view.addSubview(enableNormalizationLabel)
-        //        view.addSubview(enableNormalizationSwitch)
-
-        // Start your layout here...
-
+        let offlineLabel = makeLabel(withText: "Offline")
+        let offlineSwitch = makeSwitch(isOn: false)
+        let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
+        
+        let crossfadeLabel = makeBoldLabel(withText: "Crossfade")
+        let crossfadeMinLabel = makeSubLabel(withText: "0s")
+        let crossfadeProgressView = makeProgressView()
+        let crossfadeMaxLabel = makeSubLabel(withText: "12s")
+        
+        
+        let gaplessPlaybackLabel = makeLabel(withText: "Gapless Playback")
+        let gaplessPlaybackSwitch = makeSwitch(isOn: true)
+        
+        let hideSongsLabel = makeLabel(withText: "Hide Unplayable Songs")
+        let hideSongsSwitch = makeSwitch(isOn: true)
+        
+        let enableNormalizationLabel = makeLabel(withText: "Enable Audio Normalization")
+        let enableNormalizationSwitch = makeSwitch(isOn: true)
+        
+        let bottomSign = makeSubLabel(withText: "@fabianskier")
+        
+        view.addSubview(offlineLabel)
+        view.addSubview(offlineSwitch)
+        view.addSubview(offlineSublabel)
+        
+        view.addSubview(crossfadeLabel)
+        view.addSubview(crossfadeMinLabel)
+        view.addSubview(crossfadeProgressView)
+        view.addSubview(crossfadeMaxLabel)
+        
+        view.addSubview(gaplessPlaybackLabel)
+        view.addSubview(gaplessPlaybackSwitch)
+        
+        view.addSubview(hideSongsLabel)
+        view.addSubview(hideSongsSwitch)
+        
+        view.addSubview(enableNormalizationLabel)
+        view.addSubview(enableNormalizationSwitch)
+        
+        view.addSubview(bottomSign)
+        
+        
+        NSLayoutConstraint.activate([
+            offlineLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: margin),
+            offlineLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            
+            offlineSwitch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: margin),
+            offlineSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+            
+            offlineSublabel.topAnchor.constraint(equalTo: offlineLabel.bottomAnchor, constant: margin),
+            offlineSublabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            offlineSublabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+            
+            crossfadeLabel.topAnchor.constraint(equalTo: offlineSublabel.bottomAnchor, constant: spacing),
+            crossfadeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            crossfadeMinLabel.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: spacing),
+            crossfadeMinLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            
+            crossfadeProgressView.centerYAnchor.constraint(equalTo: crossfadeMinLabel.centerYAnchor),
+            crossfadeProgressView.leadingAnchor.constraint(equalTo: crossfadeMinLabel.trailingAnchor, constant: 8),
+            crossfadeProgressView.trailingAnchor.constraint(equalTo: crossfadeMaxLabel.leadingAnchor, constant: -8),
+            
+            crossfadeMaxLabel.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: spacing),
+            crossfadeMaxLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+            
+            gaplessPlaybackLabel.topAnchor.constraint(equalTo: crossfadeProgressView.bottomAnchor, constant: spacing),
+            gaplessPlaybackLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            gaplessPlaybackLabel.centerYAnchor.constraint(equalTo: gaplessPlaybackSwitch.centerYAnchor),
+            
+            gaplessPlaybackSwitch.topAnchor.constraint(equalTo: crossfadeProgressView.bottomAnchor, constant: spacing),
+            gaplessPlaybackSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+            
+            hideSongsLabel.topAnchor.constraint(equalTo: gaplessPlaybackLabel.bottomAnchor, constant: spacing),
+            hideSongsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            hideSongsLabel.centerYAnchor.constraint(equalTo: hideSongsSwitch.centerYAnchor),
+            
+            hideSongsSwitch.topAnchor.constraint(equalTo: gaplessPlaybackSwitch.bottomAnchor, constant: spacing),
+            hideSongsSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+            
+            enableNormalizationLabel.topAnchor.constraint(equalTo: hideSongsLabel.bottomAnchor, constant: spacing),
+            enableNormalizationLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            enableNormalizationLabel.centerYAnchor.constraint(equalTo: enableNormalizationSwitch.centerYAnchor),
+            
+            enableNormalizationSwitch.topAnchor.constraint(equalTo: hideSongsSwitch.bottomAnchor, constant: spacing),
+            enableNormalizationSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+            
+            bottomSign.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            bottomSign.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
     }
 }
 
